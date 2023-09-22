42e9278b7669eae37bb820e2b3a1a699b63de7a9
Zhukov Kirill <zhukovk4010@gmail.com>
2023-09-22 14:11:14 +0300

    Сверстать страницу настроек и изменить цвета темы приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,13 +4,15 @@ import CommunityPage from 'src/pages/community/community-page'
 import MapPage from 'src/pages/map/map-page'
 import Tabbar from '../tabbar/tabbat'
 import ProfilePage from 'src/pages/profile/profile-page'
+import SettingsPage from 'src/pages/settings/settings'
 
 const App = () => {
     return (
         <>
             {/* <MapPage /> */}
             {/* <CommunityPage /> */}
-            <ProfilePage />
+            {/* <ProfilePage /> */}
+            <SettingsPage />
             <Tabbar />
         </>
     )
---
+++ src/pages/settings/settings.tsx
@@ -0,0 +1,13 @@
+import styles from './settings.module.css'
+
+const SettingsPage = () => {
+    return (
+        <main className={styles.settingsPage}>
+            <div>Изменить профиль</div>
+            <div>Изменить тему</div>
+            <div>Выйти из аккаунта</div>
+        </main>
+    )
+}
+
+export default SettingsPage

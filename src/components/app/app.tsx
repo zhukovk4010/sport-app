490101841f590b5cf17094c49fbba4ccc49effba
Zhukov Kirill <zhukovk4010@gmail.com>
2023-09-22 14:35:17 +0300

    Добавить навигацию в приложение

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,7 @@
 // import styles from './App.css'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import CommunityPage from 'src/pages/community/community-page'
 import MapPage from 'src/pages/map/map-page'
 import Tabbar from '../tabbar/tabbat'
@@ -7,13 +9,47 @@ import ProfilePage from 'src/pages/profile/profile-page'
 import SettingsPage from 'src/pages/settings/settings'
 
 const App = () => {
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: (
+                <>
+                    <MapPage />
+                    <Tabbar />
+                </>
+            ),
+        },
+        {
+            path: '/community',
+            element: (
+                <>
+                    <CommunityPage />
+                    <Tabbar />
+                </>
+            ),
+        },
+        {
+            path: '/profile',
+            element: (
+                <>
+                    <ProfilePage />
+                    <Tabbar />
+                </>
+            ),
+        },
+        {
+            path: '/settings',
+            element: (
+                <>
+                    <SettingsPage />
+                    <Tabbar />
+                </>
+            ),
+        },
+    ])
     return (
         <>
-            {/* <MapPage /> */}
-            {/* <CommunityPage /> */}
-            {/* <ProfilePage /> */}
-            <SettingsPage />
-            <Tabbar />
+            <RouterProvider router={router} />
         </>
     )
 }
--- src/components/tabbar/tabbat.tsx
+++ src/components/tabbar/tabbat.tsx
@@ -1,28 +1,31 @@
+import { Link } from 'react-router-dom'
+
 import iconMap from 'src/assets/icons/map-icon.svg'
 import iconСommunity from 'src/assets/icons/community-icon.svg'
 import iconProfile from 'src/assets/icons/profile-icon.svg'
 import iconSettings from 'src/assets/icons/settings-icon.svg'
+
 import styles from './tabbat.module.css'
 
 const Tabbar = () => {
     return (
         <nav className={styles.tabbar}>
-            <button>
+            <Link to='/' className={styles.tabbar__link}>
                 <img src={iconMap} alt='' />
                 <h5>Карта</h5>
-            </button>
-            <button>
+            </Link>
+            <Link to='/community' className={styles.tabbar__link}>
                 <img src={iconСommunity} alt='' />
                 <h5>Сообщество</h5>
-            </button>
-            <button>
+            </Link>
+            <Link to='/profile' className={styles.tabbar__link}>
                 <img src={iconProfile} alt='' />
                 <h5>Профиль</h5>
-            </button>
-            <button>
+            </Link>
+            <Link to='/settings' className={styles.tabbar__link}>
                 <img src={iconSettings} alt='' />
                 <h5>Найстройки</h5>
-            </button>
+            </Link>
         </nav>
     )
 }

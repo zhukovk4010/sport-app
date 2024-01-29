dc3a2d8aeb76f1ae36245d2089290abccb2a60e9
Zhukov Kirill <zhukovk4010@gmail.com>
2024-01-29 11:27:15 +0300

    Удалить firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './redux'
-import App from './components/app/app'
-import './index.pcss'
-import './firebase'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux';
+import App from './components/app/app';
+import './index.pcss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- src/pages/register-page/register-page.tsx
+++ src/pages/register-page/register-page.tsx
@@ -1,17 +1,8 @@
-import { Link } from 'react-router-dom'
-import Register from 'src/components/firebase/register/register'
 
-import styles from './register-page.module.pcss'
 
 const RegisterPage = () => {
     return (
-        <main className={styles.registerPage}>
-            <h2>Регистрация</h2>
-            <Register />
-            <p>
-                Уже зарегестрированы? <Link to='/login'>Войти</Link>
-            </p>
-        </main>
+        <div></div>
     )
 }
 

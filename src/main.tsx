7b6dced6b2e7fc2854df0fb1f24604edd8032abc
Zhukov Kirill <zhukovk4010@gmail.com>
2024-01-29 11:44:43 +0300

    Добавить reset стайлов

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux';
 import App from './components/app/app';
+import './reset.pcss';
 import './index.pcss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
